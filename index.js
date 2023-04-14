d4f1d54e5e25e0fb599a42ae7150f98385c08fa3
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-04-14 09:17:16 +0300

    implemented and filtered for habitable planets in data set

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    // criteria based on https://www.centauri-dreams.org/2015/01/30/a-review-of-the-best-habitable-planet-candidates/
+    return planet['koi_disposition'] === 'CONFIRMED'
+        // stellar flux as another criteria for habitable planets
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        // planet size should be below x1.6 earth radii
+        && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
     // connect streams (parse === destination of source)
@@ -12,13 +21,15 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results); 
+        console.log(`${habitablePlanets.length} habitable planets found!`); 
         console.log('done');
     });
 // returns event emitter
