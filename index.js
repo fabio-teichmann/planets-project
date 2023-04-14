fe4f798ff50d4f038bdd7d445fe617f5c61e0485
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-04-14 09:02:30 +0300

    renamed data file; set up fs.createReadStream

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler-data.csv')
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results); 
+        console.log('done');
+    });
+// returns event emitter
+// parse();
